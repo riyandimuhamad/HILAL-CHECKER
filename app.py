d89fe825d18ea3998de4521498844b89fbd07318
riyandimuhamad <riyandiiimuhammad@gmail.com>
2026-03-11 14:10:28 +0700

    Final Hosting

--- app.py
+++ app.py
@@ -36,4 +36,6 @@ def predict_location():
         return jsonify({"error": f"Backend Error: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=5000)
